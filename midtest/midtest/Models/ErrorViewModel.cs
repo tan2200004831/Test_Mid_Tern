a64b8069f1fd5c89558f5cbe47a0f5d4c6ef20ed
PhuongNam20102003 <huynhphuongnam744@gmail.com>
2025-07-24 23:00:13 +0700

    Add files via upload

---
+++ midtest/midtest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace midtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
